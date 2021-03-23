a5492c71a74161d54cab88b84be21f01991b9caf
aryajur <milind.gupta@gmail.com>
2021-03-23 00:39:48 -0700

    Improved the parer module to calculate Efficiency and Energy and parse using Binary Search

--- src/ltspice/waveparser.lua
+++ src/ltspice/waveparser.lua
@@ -19,7 +19,7 @@ local M = {}
 package.loaded[...] = M
 _ENV = M		-- Lua 5.2+
 
-_VERSION = "1.21.03.05"	
+_VERSION = "1.21.03.23"	
 
 -- File handling functions
 
@@ -54,37 +54,95 @@ local function read_transient(fs,vars,start,stop,maxpoints)
 		return nil, "File already closed"
 	end
 	local offset = {}	-- To store the data offsets
-	local varList = {}
+	-- Each offset should correspond to the vars entry
+	for i = 1,#vars do
+		offset[i] = 0
+	end
+	
 	for i = #vars,1,-1 do
 		local found
 		for j = 1,#fs.vars do
 			if fs.vars[j] == vars[i] then
 				found = true
-				offset[#offset + 1] = j-1
-				
+				offset[i] = j-1
 				break
 			end
 		end
 		if not found then
 			table.remove(vars,i)
+			table.remove(offset,i)
 		end
 	end
-	table.sort(offset)
 	for i = 1,#offset do
-		varList[#varList + 1] = fs.vars[offset[i]]
 		data[i] = {}
 	end
-	fH:seek("set",fs.datapos)
+	-- Do a binary search to find starting time point
 	local chunkSize = (#fs.vars-1)*4+8 -- 4 bytes of data and 8 bytes for time
-	local dat,time,pts
+	local st = fs.datapos
+	local stp = fs.datapos + (fs.numpts-1)*chunkSize
+	local function getTime(pos)
+		fH:seek("set",pos)
+		local dat = fH:read(chunkSize)
+		return math.abs(vstruct.read("f8",dat:sub(1,8))[1])
+	end
+	-- First either start or stop should lie in the file data
+	local t1,t2,tmid,pts,midpos
+	pts = fs.numpts
+	t1 = getTime(st)
+	t2 = getTime(stp)
+	if not (start >= t1 and start <= t2) and not (stop >= t1 and stop <= t2) then
+		return data		-- start - stop range does not overlap with t1 - t2
+	end
+	-- Update start/stop with the range that that exists in t1-t2
+	if start < t1 then 
+		start = t1
+	end
+	if stop > t2 then
+		stop = t2
+	end
+	-- Now find the data position with first time >= start
+	local found
+	while not found do
+		if start == t1 then
+			found = st
+			break
+		end
+		if start == t2 or pts == 2 then
+			found = stp
+			break
+		end
+		midpos = st+(math.ceil(pts/2)-1)*chunkSize
+		tmid = getTime(midpos)
+		if start == tmid then
+			found = midpos
+			break
+		end
+		if start > tmid then
+			st = midpos
+			t1 = tmid
+			pts = pts - (math.ceil(pts/2)-1)
+		else
+			stp = midpos
+			t2 = tmid
+			pts = math.ceil(pts/2)
+		end
+	end
+	-- found contains the point from where the data has to be extracted
+	--found = fs.datapos
+	fH:seek("set",found)
+	local dat,time,done
 	pts = 0
-	for i = 1,fs.numpts do
+	local tData = data[0]
+	while not done do
 		dat = fH:read(chunkSize)
+		if not dat then break end
 		time = math.abs(vstruct.read("f8",dat:sub(1,8))[1])
-		if time >= start and time <= stop then
+		if time > stop then break end
+		if time >= start then
 			pts = pts + 1
-			data[0][pts] = time
-			for j = 1,#offset do
+			tData[pts] = time
+			-- Now read all the variable data
+			for j = 1,#vars do
 				data[j][pts] = vstruct.read("f4",dat:sub(9+(offset[j]-1)*4,12+(offset[j]-1)*4))[1]
 			end
 			if maxpoints and pts >= maxpoints then
@@ -92,7 +150,55 @@ local function read_transient(fs,vars,start,stop,maxpoints)
 			end
 		end
 	end	
-	return varList,data
+	return data
+end
+
+-- Function to calculate the average energy supplied by integrating the power from t[start] to t[stop]
+-- fs is the file structure
+-- Vp is the array containing the +ve node voltages
+-- Vn is the array containing the -ve node voltages (OPTIONAL). If not given this is assumed as ground.
+-- I is the array containing the current
+-- t is the array containing the time points
+-- start is the start index from where the integral is to be calculated (default = 1)
+-- stop is the stop index up till where the integral will be done (default = #t)
+function getEnergy(fs,t,Vp,Vn,I,start,stop)
+	local E = 0
+	local Pi,Pim1
+	start = start or 1
+	stop = stop or #t
+	local function fPi1(i,Vp,Vn,I)
+		return (Vp[i]-Vn[i])*I[i]
+	end
+	local function fPi2(i,Vp,Vn,I)
+		return Vp[i]*I[i]
+	end
+	local fPi = fPi1
+	if not Vn then
+		fPi = fPi2
+	end
+	for i = start+1,stop do
+		Pi = fPi(i,Vp,Vn,I)
+		Pim1 = fPi(i-1,Vp,Vn,I)
+		E = E + 0.5*(Pi+Pim1)*(t[i]-t[i-1])
+	end
+	return E
+end
+
+-- Function to calculate the efficiency from a transient simulation
+-- fs is the file structure
+-- inv is an array containing input voltage data
+-- ini is an array containing the input current data
+-- outv is an array containing the output voltage data
+-- outi is an array containing the output current data
+-- time is an array containing the corresponding time points
+function getEfficiency(fs,time,inv,ini,outv,outi)
+	-- Now calculate the input Power
+	local Pin = getEnergy(fs,time,inv,nil,ini)
+	Pin = Pin/(time[#time]-time[1])
+	-- Now calculate the output Power
+	local Pout = getEnergy(fs,time,outv,nil,outi)
+	Pout = Pout/(time[#time]-time[1])
+	return Pout/Pin,Pout,Pin
 end
 
 --------------------------------------------------------------------------
@@ -232,6 +338,8 @@ rawParser = function(filename)
 	end
 	if fs.mode == "Transient" then
 		fsmeta.__index.read = read_transient
+		fsmeta.__index.getEfficiency = getEfficiency
+		fsmeta.__index.getEnergy = getEnergy
 	elseif fs.mode == "AC" then
 		return nil,"AC file reading not added yet"
 	elseif fs.mode == "FFT" then
--- test/testtran.lua
+++ test/testtran.lua
@@ -1,12 +1,30 @@
 -- Ltspice tester
 require("submodsearcher")
-lt = require("ltspice")
+lt = require("ltspice.waveparser")
 
 fs,msg = lt.rawParser("RCtran.raw")
-v,d= fs:read({"V(out)"},0,1)
+d= fs:read({"V(out)"},0,1)
 lp = require("lua-plot")
 
 p = lp.plot{}
 p:AddSeries(d[0],d[1])
 p:Show()
 io.read()
+
+-- Do the energy measurements
+-- Get V(in), I(V1), V(out)
+vars = {"V(in)","V(out)","I(V1)"}
+d = fs:read(vars,0,1)
+
+-- Find the input energy
+Ein = fs:getEnergy(d[0],d[1],nil,d[3])
+
+-- Energy dissipated in the resistor
+Er = fs:getEnergy(d[0],d[1],d[2],d[3])
+
+-- Energy going into the capacitor
+Ec = fs:getEnergy(d[0],d[2],nil,d[3])
+
+print("Input Energy:",Ein)
+print("Resistor Energy:",Er)
+print("Capacitor Energy:",Ec)
